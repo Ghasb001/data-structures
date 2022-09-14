c3a992c367b9ce833b769cab9fa9badbb712ffc0
Guillermo Hasbun <hasbun.guillermo@gmail.com>
2022-09-13 22:07:22 -0400

    Complete functional stack and queue

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -1,5 +1,10 @@
 var Queue = function() {
-  var someInstance = {};
+
+  //stores my methods
+  var someInstance = {
+    first: 0,
+    last: 0
+  }
 
   // Use an object with numeric keys to store values
   var storage = {};
@@ -7,12 +12,19 @@ var Queue = function() {
   // Implement the methods below
 
   someInstance.enqueue = function(value) {
+      storage[someInstance.last] = value;
+      someInstance.last++;
   };
 
   someInstance.dequeue = function() {
+    var item = storage[someInstance.first];
+    delete storage[someInstance.first];
+    someInstance.first++;
+    return item;
   };
 
   someInstance.size = function() {
+    return Object.keys(storage).length;
   };
 
   return someInstance;
--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -1,17 +1,30 @@
 var Stack = function() {
-  var someInstance = {};
+  var someInstance = {
+    first: 0,
+    last: 0
+  };
 
   // Use an object with numeric keys to store values
   var storage = {};
 
   // Implement the methods below
   someInstance.push = function(value) {
+    storage[someInstance.last] = value;
+    someInstance.last++;
   };
 
   someInstance.pop = function() {
+    // pop can only run if there is data available
+    if (someInstance.last > 0) {
+      someInstance.last--;
+      var item = storage[someInstance.last];
+      delete storage[someInstance.last];
+      return item;
+    }
   };
 
   someInstance.size = function() {
+    return Object.keys(storage).length;
   };
 
   return someInstance;
