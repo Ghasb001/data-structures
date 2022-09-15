b0c996d27c295225f66452043df8990dd1f2e263
Guillermo Hasbun <hasbun.guillermo@gmail.com>
2022-09-14 22:47:40 -0400

    Complete pseudoclassical stack and queue

--- sprint-one/src/pseudoclassical/queue.js
+++ sprint-one/src/pseudoclassical/queue.js
@@ -1,6 +1,26 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  this.first = 0;
+  this.last = 0;
 };
 
+Queue.prototype.enqueue = function(value) {
+  this[this.last] = value;
+  this.last++;
+};
 
+Queue.prototype.dequeue = function() {
+  var item = this[this.first];
+  delete this[this.first];
+  this.first++;
+  return item;
+};
+
+Queue.prototype.size = function() {
+  if (this.last >= this.first) {
+    return this.last - this.first;
+  } else {
+    return 0;
+  }
+};
--- sprint-one/src/pseudoclassical/stack.js
+++ sprint-one/src/pseudoclassical/stack.js
@@ -1,6 +1,28 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  this.first = 0;
+  this.last = 0;
 };
 
+Stack.prototype.push = function(value) {
+  this[this.last] = value;
+  this.last++;
+};
 
+Stack.prototype.pop = function() {
+if (this.last > 0) {
+      this.last--;
+      var item = this[this.last];
+      delete this[this.last];
+      return item;
+    }
+};
+
+Stack.prototype.size = function() {
+  if (this.last >= this.first) {
+    return this.last - this.first;
+  } else {
+    return 0;
+  }
+};
