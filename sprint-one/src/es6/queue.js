325f451eabbfb5f97f342e763fc216837409319e
Guillermo Hasbun <hasbun.guillermo@gmail.com>
2022-09-14 23:05:46 -0400

    Complete sprint one

--- sprint-one/src/es6/queue.js
+++ sprint-one/src/es6/queue.js
@@ -1,7 +1,30 @@
 class Queue {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+
   constructor() {
+    this.first = 0;
+    this.last = 0;
   }
+
+  enqueue(value) {
+    this[this.last] = value;
+    this.last++;
+  };
+
+  dequeue() {
+    var item = this[this.first];
+    delete this[this.first];
+    this.first++;
+    return item;
+  };
+
+  size() {
+    if (this.last >= this.first) {
+      return this.last - this.first;
+    } else {
+      return 0;
+    }
+  };
 
 }
--- sprint-one/src/es6/stack.js
+++ sprint-one/src/es6/stack.js
@@ -2,6 +2,30 @@ class Stack {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
   constructor() {
+    this.first = 0;
+    this.last = 0;
+  }
+
+  push(value) {
+    this[this.last] = value;
+    this.last++;
+  }
+
+  pop() {
+    if (this.last > 0) {
+      this.last--;
+      var item = this[this.last];
+      delete this[this.last];
+      return item;
+    }
+  }
+
+  size() {
+    if (this.last >= this.first) {
+      return this.last - this.first;
+    } else {
+      return 0;
+    }
   }
 
 }
