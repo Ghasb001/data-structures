d6f4c26ef780af7fea044b57003dc44d3731733b
Guillermo Hasbun <hasbun.guillermo@gmail.com>
2022-09-20 21:38:02 -0400

    Complete Tree

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -1,19 +1,36 @@
 var Tree = function(value) {
   var newTree = {};
   newTree.value = value;
-
   // your code here
-  newTree.children = null;  // fix me
-
+  newTree.children = [];  // fix me
+  newTree.addChild = treeMethods.addChild;
+  newTree.contains = treeMethods.contains;
   return newTree;
 };
 
 var treeMethods = {};
 
 treeMethods.addChild = function(value) {
+  //console.log(value);
+  var child = new Tree(value);
+  this.children.push(child);
 };
 
 treeMethods.contains = function(target) {
+  var isChild = false;
+  var doesContain = function (child) {
+    if (child.value === target) {
+      isChild = true;
+    }
+    else {
+      for (var i = 0; i < child.children.length; i++) {
+        doesContain(child.children[i]);
+
+      }
+    }
+  }
+  doesContain(this)
+ return isChild;
 };
 
 
