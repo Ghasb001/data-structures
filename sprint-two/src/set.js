b7702927b809ffbcf62bb95ce77d81d724e09e8a
Guillermo Hasbun <hasbun.guillermo@gmail.com>
2022-09-21 23:22:13 -0400

    Complete Set

--- sprint-two/src/set.js
+++ sprint-two/src/set.js
@@ -1,18 +1,28 @@
 var Set = function() {
   var set = Object.create(setPrototype);
-  set._storage = null; // fix me
+  set._storage = {}; // fix me
   return set;
 };
 
 var setPrototype = {};
 
 setPrototype.add = function(item) {
+  //console.log(this)
+  //console.log(this._storage[item]);
+  this._storage[item] = item;
 };
 
 setPrototype.contains = function(item) {
+  if (this._storage[item] === item) {
+    return true;
+  }
+  return false;
 };
 
 setPrototype.remove = function(item) {
+  if(this._storage[item] !== undefined){
+    delete this._storage[item];
+    }
 };
 
 /*
